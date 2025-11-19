e118ee4c95c5132b0d28471ad08f366f3559f48e
zugafa <zugafa@users.noreply.github.com>
2025-11-19 18:57:32 +0200

    v0.1.2 (post exception, testare main)

--- EroareValidare.cpp
+++ EroareValidare.cpp
@@ -1,24 +1,26 @@
-//
-// Created by nitui on 11/19/2025.
-//
-
 #include "EroareValidare.h"
 
-EroareValidare::EroareValidare(const std::string& cont, int len, int min) : ScriptException(
-                                                                                "Validare esuata pentru contul '" + cont
-                                                                                + "' (Lungime)."),
-                                                                            lungimePrimita(len),
-                                                                            lungimeMinima(min),
-                                                                            esteEroareContinut(false)
+// Modificăm constructorul pentru a fi mai blând cu stiva
+EroareValidare::EroareValidare(const std::string& cont, int len, int min)
+    : ScriptException("Eroare validare lungime") // Mesaj scurt inițial
 {
+    // Construim mesajul detaliat AICI, nu în lista de inițializare
+    mesajEroare = "Validare esuata pentru contul '" + cont + "' (Lungime " +
+        std::to_string(len) + " vs " + std::to_string(min) + ").";
+
+    lungimePrimita = len;
+    lungimeMinima = min;
+    esteEroareContinut = false;
 }
 
-EroareValidare::EroareValidare(const std::string& cont, const std::string& motiv) : ScriptException(
-        "Validare esuata pentru contul '" + cont + "': " + motiv),
-    lungimePrimita(0),
-    lungimeMinima(0),
-    esteEroareContinut(true)
+EroareValidare::EroareValidare(const std::string& cont, const std::string& motiv)
+    : ScriptException("Eroare validare continut") // Mesaj scurt inițial
 {
+    mesajEroare = "Validare esuata pentru contul '" + cont + "': " + motiv;
+
+    lungimePrimita = 0;
+    lungimeMinima = 0;
+    esteEroareContinut = true;
 }
 
 std::string EroareValidare::getSugestie() const
